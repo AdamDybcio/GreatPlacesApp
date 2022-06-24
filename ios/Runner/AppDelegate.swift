d99f8cb354f82e9c28c30b282c69e7731b7f48ee
FuzionOrigins <nieznam2016@gmail.com>
2022-06-25 01:31:53 +0200

    GreatPlacesApp 1.0.0

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,6 +1,7 @@
 import UIKit
 import Flutter
 import GoogleMaps
+import FlutterConfigPlugin.h
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -8,7 +9,7 @@ import GoogleMaps
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GMSServices.provideAPIKey("AIzaSyCDM4qpyhMDvvxOUBAiFv8yK0Zz8flaihc")
+    GMSServices.provideAPIKey([FlutterConfigPlugin envFor:@"GOOGLE_API_KEY"])
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
